721ca3452c28edc101d0f6d1162755748a6bde82
pyxn <51064667+pyxn@users.noreply.github.com>
2024-06-28 15:05:02 -0400

    add player controller visuals and script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -31,9 +31,9 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-        Debug.Log(inputVector);
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
